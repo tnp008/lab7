ee9705f77f0887f10ba7cca4af19123c09c9a23d
TIN PHAN <tnphan4@gmail.com>
2020-02-20 13:27:42 -0800

    Add files via upload

---
+++ introHCI.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+});
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$(".likeBtn").click(clickLikeBtn);
+}
+
+function clickLikeBtn(e) {
+	e.preventDefault();
+	ga('create', 'UA-158836097-1', 'auto');
+	ga('send', 'event', 'like', 'click');
+}
