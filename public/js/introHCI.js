40465666828c6698a2f928f1eee7768cc1f1854c
Tin Phan <tnphan4@gmail.com>
2020-02-20 13:49:48 -0800

    Completed

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,11 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(clickLikeBtn);
+}
+
+function clickLikeBtn(e) {
+	e.preventDefault();
+	ga('create', 'UA-158836097-1', 'auto');
+	ga('send', 'event', 'like', 'click');
 }
